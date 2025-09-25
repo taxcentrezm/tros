70e15687698d0ac1d9d3b610689f4ba6548bfd03
taxcentrezm <taxcentrezm@gmail.com>
2025-09-25 15:49:30 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,4 +4,7 @@ import { createClient } from "@libsql/client";
 export const client = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
+  // 👇 Disable migrations, only run raw SQL
+  syncUrl: undefined,
+  syncInterval: 0,
 });
