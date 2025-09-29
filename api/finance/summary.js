3aad433ced71d1ef1f7a16d7f66c7aef743f7ba2
taxcentrezm <taxcentrezm@gmail.com>
2025-09-29 10:28:11 +0200

    Update summary.js

--- api/finance/summary.js
+++ api/finance/summary.js
@@ -1,9 +1,4 @@
-import { createClient } from "@libsql/client";
-
-const client = createClient({
-  url: process.env.TURSO_DB_URL,
-  authToken: process.env.TURSO_DB_AUTH_TOKEN,
-});
+import { client } from "../../db.js";
 
 export default async function handler(req, res) {
   try {
